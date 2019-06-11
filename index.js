81890a3815ed4e521ca0c8103c71aa40e7dc1fcf
Daniel Schneider <daschne8@gmail.com>
2019-06-11 21:44:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const gifts = ["teddy bear", "drone", "doll"];
-
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+function writeCards(names){
+  let thank_yous = []
+  for(let i = 0; i<names.length; i++){
+    thank_yous.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
   }
-
-  return gifts;
 }
